557e67fa1a5a47c36491e3dfdfc6ed27a8f7f475
nagmabano <bano.nagma.1@gmail.com>
2018-06-30 22:50:21 +0530

    passing data with event and property binding(combined)

--- RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -7,6 +7,10 @@ import { Recipe } from "../recipe.model";
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent{
+
+    @Output()
+    recipeWasSelected = new EventEmitter<Recipe>();
+
     recipes: Recipe[] = [
         new Recipe('A test Recipe','Test recipe description',
         'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fimage%2F2016%2F01%2Fmain%2F1601p25-skillet-chicken-roasted-potatoes-carrots.jpg%3Fitok%3DMcbboinY&w=800&q=85'),
@@ -14,6 +18,10 @@ export class RecipeListComponent{
         'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fimage%2F2016%2F01%2Fmain%2F1601p25-skillet-chicken-roasted-potatoes-carrots.jpg%3Fitok%3DMcbboinY&w=800&q=85'),
         
     ];
+
+    onRecipeSelected(recipe: Recipe){
+        this.recipeWasSelected.emit(recipe);
+    }
 
 
 }
