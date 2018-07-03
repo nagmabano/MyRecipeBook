21b80da07ff09898c289a691901cdfd36f255da8
nagmabano <bano.nagma.1@gmail.com>
2018-07-04 01:40:31 +0530

    added ingredients to shopping list

--- RecipeBook/src/app/recipes/recipe.service.ts
+++ RecipeBook/src/app/recipes/recipe.service.ts
@@ -1,21 +1,37 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
-
+@Injectable()
 export class RecipeService{
 
     recipeSelected = new EventEmitter<Recipe>();
 
-    recipes: Recipe[] = [
-        new Recipe('A test Recipe','Test recipe description',
-        'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fimage%2F2016%2F01%2Fmain%2F1601p25-skillet-chicken-roasted-potatoes-carrots.jpg%3Fitok%3DMcbboinY&w=800&q=85'),
-        new Recipe('A test Recipe1','Test recipe description1',
-        'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fimage%2F2016%2F01%2Fmain%2F1601p25-skillet-chicken-roasted-potatoes-carrots.jpg%3Fitok%3DMcbboinY&w=800&q=85'),
+    private recipes: Recipe[] = [
+        new Recipe('Chicken Fry','A super tasty chicken dish.',
+        'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fimage%2F2016%2F01%2Fmain%2F1601p25-skillet-chicken-roasted-potatoes-carrots.jpg%3Fitok%3DMcbboinY&w=800&q=85',
+        [
+            (new Ingredient('Chicken',1)),
+            (new Ingredient('sauce',1)),
+        ]),
+        new Recipe('Momos','Yum Momos for hungry Tummy',
+        'http://media3.sailusfood.com/wp-content/uploads/2016/03/veg-momos-recipe.jpg',
+        [
+            (new Ingredient('Maida', 1/2)),
+            (new Ingredient('veggies',2))
+        ]),
         
     ];
 
+    constructor(private shoppingListService: ShoppingListService){}
+
     getRecipes(){
         return this.recipes.slice();
+    }
+
+    addIngredientToShoppingList(ingredients: Ingredient[]){
+        this.shoppingListService.addIngredients(ingredients);
     }
 
 }
--- RecipeBook/src/app/shopping-list/shopping-list.service.ts
+++ RecipeBook/src/app/shopping-list/shopping-list.service.ts
@@ -14,8 +14,16 @@ export class ShoppingListService{
         return  this.ingredients.slice();
       }
 
-      addIngredient(ingredient: Ingredient){
-          this.ingredients.push(ingredient);
+      addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientChanged.emit(this.ingredients.slice());
+      }
+
+      addIngredients(ingredients: Ingredient[]){
+        //for (let ingredient of ingredients){
+        //  this.addIngredient(ingredient);
+        //}
+          this.ingredients.push(...ingredients);
           this.ingredientChanged.emit(this.ingredients.slice());
       }
 }
