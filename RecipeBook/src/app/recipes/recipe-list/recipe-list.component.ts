ce418e9acc3856d00919b67ab71c6760d7b194ab
nagmabano <bano.nagma.1@gmail.com>
2018-07-14 19:24:43 +0530

    routing to edit

--- RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output, OnInit } from "@angular/core";
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
     selector: 'app-recipe-list',
@@ -14,7 +15,9 @@ export class RecipeListComponent implements OnInit{
 
     recipes: Recipe[];
 
-    constructor(private recipeService: RecipeService){}
+    constructor(private recipeService: RecipeService,
+                private router: Router,
+                private route: ActivatedRoute){}
 
     ngOnInit(){
         this.recipes = this.recipeService.getRecipes();
@@ -23,6 +26,10 @@ export class RecipeListComponent implements OnInit{
     /* onRecipeSelected(recipe: Recipe){
         this.recipeWasSelected.emit(recipe);
     } */
+
+    onNewRecipe(){
+        this.router.navigate(['new'],{relativeTo: this.route});
+    }
 
 
 }
