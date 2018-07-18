6a962f71133a149db7f6265e0359484d923ed7e6
nagmabano <bano.nagma.1@gmail.com>
2018-07-18 19:30:55 +0530

    added signup and signin with jwt tokens

--- RecipeBook/src/app/app.component.ts
+++ RecipeBook/src/app/app.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   loadedFeature:string = 'recipe';
 
   
   onNavigate(feature: string){
     this.loadedFeature = feature;
+  }
 
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyAc_jDnp3O1aSAb8atCX2NLiwD_VoKoEzU",
+      authDomain: "ng-recipe-book-9a4ab.firebaseapp.com",
+    })
   }
 }
--- RecipeBook/src/app/app.module.ts
+++ RecipeBook/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { HttpModule } from '@angular/http';
 import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
+import { AuthService } from './auth/auth.service';
 
 
 
@@ -47,7 +48,7 @@ import { SigninComponent } from './auth/signin/signin.component';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
