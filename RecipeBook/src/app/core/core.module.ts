f2c69e849a3d0f777a807dbae27a61d4e16f2f76
nagmabano <bano.nagma.1@gmail.com>
2018-08-16 15:09:05 +0530

    service restructuring

--- RecipeBook/src/app/core/core.module.ts
+++ RecipeBook/src/app/core/core.module.ts
@@ -4,6 +4,11 @@ import { HeaderComponent } from "./Header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { AppRoutingModule } from "../app-routing.module";
 import { SharedModule } from "../shared/shared.module";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { RecipeService } from "../recipes/recipe.service";
+import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+
 
 @NgModule({
     declarations: [
@@ -17,6 +22,13 @@ import { SharedModule } from "../shared/shared.module";
     exports: [
         AppRoutingModule,
         HeaderComponent
+    ],
+    providers: [
+        ShoppingListService,
+        RecipeService,
+        DataStorageService,
+        AuthService
+       
     ]
 })
 export class CoreModule{
