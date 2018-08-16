73a00ab4998d5715394299dd7326dacfac84c4b5
nagmabano <bano.nagma.1@gmail.com>
2018-08-16 11:39:32 +0530

    auth feature module

--- RecipeBook/src/app/app.component.ts
+++ RecipeBook/src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
   ngOnInit(){
     firebase.initializeApp({
       apiKey: "apiKey",
-      authDomain: "password",
+      authDomain: "domainName",
     })
   }
 }
--- RecipeBook/src/app/recipes/recipes.module.ts
+++ RecipeBook/src/app/recipes/recipes.module.ts
@@ -9,7 +9,6 @@ import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { CommonModule } from "@angular/common";
 import { RecipesRoutingModule } from "./recipes-routing.module";
-import { DropdownDirective } from "../shared/dropdown.directive";
 import { SharedModule } from "../shared/shared.module";
 
 
