5b44da5f608cdaff683b66b7a2e7d8b37a10c1a1
nagmabano <bano.nagma.1@gmail.com>
2018-08-18 16:35:51 +0530

    progress

--- RecipeBook/src/app/shared/data-storage.service.ts
+++ RecipeBook/src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
 
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
@@ -16,12 +16,17 @@ export class DataStorageService{
         const token = this.authService.getToken();
         //const header = new HttpHeaders().set('authorization' , 'hdbvhdvhvdbhf'); 
         
-       return this.httpClient.put('https://ng-recipe-book-9a4ab.firebaseio.com/recipes.json',this.recipeService.getRecipes(),{
-          observe: 'body',
-          params: new HttpParams().set('auth', token)
-          //headers: header  // it wont work with firebase but generally it could be a way
-       });
-    }
+    //    return this.httpClient.put('https://ng-recipe-book-9a4ab.firebaseio.com/recipes.json',this.recipeService.getRecipes(),{
+    //       observe: 'body',
+    //       params: new HttpParams().set('auth', token)
+    //       //headers: header  // it wont work with firebase but generally it could be a way
+    //    });
+
+        const req = new HttpRequest('PUT', 'https://ng-recipe-book-9a4ab.firebaseio.com/recipes.json', 
+        this.recipeService.getRecipes(), {reportProgress: true, params: new HttpParams().set('auth', token)}
+    )
+        return this.httpClient.request(req);
+};
 
     fetchRecipes(){
         const token = this.authService.getToken();
