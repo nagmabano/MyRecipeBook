0efef2485f7340a323e74bb42dfff670ff833d32
nagmabano <bano.nagma.1@gmail.com>
2018-09-10 17:35:21 +0530

    reducer and action

---
+++ RecipeBook/src/app/shopping-list/store/shopping-list.reducers.ts
@@ -0,0 +1,24 @@
+import * as ShoppingListActions from './shopping-list.actions';
+import { Ingredient } from '../../shared/ingredient.model';
+
+export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+
+const initialState = {
+    ingredients: [
+        new Ingredient('Apple',4),
+        new Ingredient('Tomatoes',10),
+    ]
+}
+
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions){
+    switch(action.type){
+        case ShoppingListActions.ADD_INGREDIENT:
+            return{
+                ...state,
+                ingredients: [...state.ingredients, action.payload]
+            }
+        default:
+            return state;
+    }
+   
+}
