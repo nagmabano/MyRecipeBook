8a58cea9f943c27e299f597761b586bb00b81e8c
nagmabano <bano.nagma.1@gmail.com>
2018-09-12 15:32:56 +0530

    Auth Reducers and Action

--- RecipeBook/src/app/auth/store/auth.reducers.ts
+++ RecipeBook/src/app/auth/store/auth.reducers.ts
@@ -1,3 +1,6 @@
+import * as AuthActions from './auth.actions';
+
+
 export interface State{
     token: string;
     authenticated: boolean
@@ -8,6 +11,22 @@ const initialState: State = {
     authenticated: false
 };
 
-export function AuthReducer(state = initialState, action){
+export function AuthReducer(state = initialState, action: AuthActions.AuthActions){
+    switch(action.type){
+        case(AuthActions.SIGNUP):
+        case(AuthActions.SIGNIN):
+            return{
+                ...state,
+                authenticated: true
+            };
+        case(AuthActions.LOGOUT):
+            return{
+                ...state,
+                token: null,
+                authenticated: false
+            };
+        default:
+            return state;
+    }
     return state;
 }
