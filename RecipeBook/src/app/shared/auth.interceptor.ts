40a789847f96b6e8e920d0743e0497b99cb94d8b
nagmabano <bano.nagma.1@gmail.com>
2018-09-12 17:44:56 +0530

    getting state access in HttpInterceptor

--- RecipeBook/src/app/shared/auth.interceptor.ts
+++ RecipeBook/src/app/shared/auth.interceptor.ts
@@ -2,18 +2,30 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/c
 import { Observable } from "rxjs";
 import { AuthService } from "../auth/auth.service";
 import { Injectable } from "@angular/core";
+import { Store } from "@ngrx/store";
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
+import 'rxjs/add/operator/switchMap';
+
 
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor{
 
-    constructor(private authService: AuthService){}
+    constructor(
+        //private authService: AuthService
+          private store: Store<fromApp.AppState>){}
 
     intercept(req:HttpRequest<any>, next :HttpHandler): Observable<HttpEvent<any>>{
         console.log('Intercepted!!!',req);
         // const copiedReq = req.clone({headers: req.headers.set('', '')});
-        const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
-        return next.handle(copiedReq);
+        //const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
+        return this.store.select('auth')
+                .switchMap((authState: fromAuth.State) =>{
+                    const copiedReq = req.clone({params: req.params.set('auth', authState.token)});
+                    return next.handle(copiedReq);
+                }) 
+        
         //return null;
     }
 
